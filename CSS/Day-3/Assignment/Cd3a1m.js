058b99452b4a7aaabdd98b4c836d0d44415d5b62
RuchitPatel11 <ruchit.m.patel.302@gmail.com>
2022-08-21 17:26:25 +0530

    Tutorial-Site-Updated

--- CSS/Day-3/Assignment/Cd3a1m.js
+++ CSS/Day-3/Assignment/Cd3a1m.js
@@ -1,17 +1,10 @@
-const toggle = document.querySelector(".toggle")
-toggle.addEventListener('click', () => {
-  const nav = document.querySelector("nav");
-  if (!nav.classList.contains("visible")) {
-    nav.classList.remove("hidden");
-    nav.classList.add("visible");
-  } else {
-    nav.classList.add("hidden");
-    nav.classList.remove("visible");
-  }
+const assToggle = document.querySelector(".toggle")
+assToggle.addEventListener('click', () => {
+  const nav = document.querySelector(".ass-nav");
+  nav.classList.toggle("visible")
 })
 
 window.onresize = () => {
   const nav = document.querySelector(".nav-item");
-  nav.classList.remove("hidden");
   nav.classList.add("visible");
 };
